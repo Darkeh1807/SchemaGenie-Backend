8921dd636b5981dd12bb347343a80726ee392c3a
Elijah Darkeh <elijahagbedam18@gmail.com>
2025-03-28 08:00:24 +0000

    update project schema to include user id

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,9 +1,21 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, SchemaTypes } from "mongoose";
 import { IProject } from "../types/project";
 
 const ProjectSchema = new Schema<IProject>({
-  title: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now },
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdBy: {
+    type: SchemaTypes.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export const Project = mongoose.model<IProject>("Project", ProjectSchema);
--- src/types/project.ts
+++ src/types/project.ts
@@ -1,4 +1,7 @@
+import mongoose, { Document } from "mongoose";
+
 export interface IProject extends Document {
-    title: string;
-    createdAt: Date;
-  }
+  title: string;
+  createdAt: Date;
+  createdBy: mongoose.Types.ObjectId;
+}
